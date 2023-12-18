c3d38e3e0d09c5ebaed724f9ca74d00d1d8bb4b2
Jean ROBERT <jean_robert92@live.fr>
2023-12-18 08:52:37 +0100

    Ajout de la classe d'initialisation du serveur.

---
+++ app.py
@@ -0,0 +1,11 @@
+from http.server import HTTPServer, SimpleHTTPRequestHandler
+
+from main.context.db_context import DBContext
+
+i = DBContext()
+
+if __name__ == "__main__":
+    server_adress = ("127.0.0.1", 8000)
+    httpd = HTTPServer(server_adress, SimpleHTTPRequestHandler)
+    print(f"Served on http://localhost:8000")
+    httpd.serve_forever()
--- context/db_context.py
+++ context/db_context.py
@@ -1,7 +1,14 @@
 from sqlmodel import Field, Session, SQLModel, create_engine, select
 
-class DBContext(object):
-    _instance = None
+class Singleton(object):
+  _instances = {}
+  def __new__(class_, *args, **kwargs):
+    if class_ not in class_._instances:
+        class_._instances[class_] = super(Singleton, class_).__new__(class_, *args, **kwargs)
+    return class_._instances[class_]
+
+class DBContext(Singleton):
+    #_instance = None
 
     sqlite_file_name = "database.db"
     sqlite_url = f"sqlite:///{sqlite_file_name}"
@@ -11,7 +18,9 @@ class DBContext(object):
 
     SQLModel.metadata.create_all(engine)
 
+    """
     def __new__(cls):
         if cls._instance is None:
             cls._instance = object.__new__(cls)
         return cls._instance
+    """
